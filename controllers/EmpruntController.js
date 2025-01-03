b3c60823c28e24812466932dadd6dc4bcbf3fd4b
isopope <nkoubah@gmail.com>
2025-01-03 22:35:34 +0100

    methode pour creer un emprunt

---
+++ controllers/EmpruntController.js
@@ -0,0 +1,45 @@
+const Emprunt = require('../models/Emprunt');
+const Utilisateur = require('../models/Utilisateur');
+const Materiel = require('../models/Materiel');
+
+exports.createEmprunt = async (request, response) => {
+    const { idUtilisateur, IdMateriel, dateDebutEmprunt, dateFinEmprunt } = request.body;
+  
+    try {
+      const utilisateur = await Utilisateur.findByPk(idUtilisateur);
+      const materiel = await Materiel.findByPk(IdMateriel);
+  
+      if (!utilisateur) {
+        return response.status(404).json({ message: 'Utilisateur non trouve' });
+      }
+  
+      if (utilisateur.roleUtilisateur !== 'EMPRUNTEUR') {
+        return response.status(403).json({ message: 'vous avez pas les permissions necessaire' });
+      }
+  
+      if (!materiel) {
+        return response.status(404).json({ message: 'Materiel non trouve' });
+      }
+  
+      if (materiel.etatMateriel !== 'DISPONIBLE') {
+        return response.status(400).json({ message: 'Materiel non disponible' });
+      }
+  
+      const emprunt = await Emprunt.create({
+        idUtilisateur,
+        IdMateriel,
+        dateDebutEmprunt,
+        dateFinEmprunt,
+        dateDemandeEmprunt: new Date()
+      });
+  
+      // mettre à jour l'état du matériel
+      materiel.etatMateriel = 'EMPRUNTER';
+      await materiel.save();
+  
+      res.status(201).json({ message: 'Emprunt creer avec succes', emprunt });
+    } catch (error) {
+      console.error('Erreur lors de la création de l\'emprunt :', error);
+      res.status(500).json({ message: 'Erreur lors de la creation de l\'emprunt', error: error.message });
+    }
+  };
